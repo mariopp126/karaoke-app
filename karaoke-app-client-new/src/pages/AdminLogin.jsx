5dbd6d810afdd221c4ed2ee86bc888ea527aa213
Mario Ernesto  Pucheu Peguero <Mario.pucheu@TSE.LOCAL>
2026-01-09 09:19:33 -0400

    Add: Logo tse on public view

--- karaoke-app-client-new/src/pages/AdminLogin.jsx
+++ karaoke-app-client-new/src/pages/AdminLogin.jsx
@@ -3,7 +3,7 @@ import { IconoMicrofono } from "../components/Icons/icons";
 function AdminLogin() {
     return (
         <div className="min-h-screen p-4 sm:p-8">
-            <h1 className="text-4xl sm:text-5xl font-extrabold text-accent-green flex items-center justify-center gap-3">
+            <h1 className="text-4xl sm:text-5xl font-extrabold text-accent-green flex items-center justify-center gap-3 mb-4">
                 <IconoMicrofono size={36} className="text-primary-light" /> Panel de Administración
             </h1>
             <FormLogin />
--- karaoke-app-client-new/src/pages/PublicView.jsx
+++ karaoke-app-client-new/src/pages/PublicView.jsx
@@ -3,6 +3,7 @@ import { FormSolicitud } from "../components/Karaoke/FormSolicitud";
 import { ColaSolicitudes } from "../components/Karaoke/ColaSolicitudes";
 import { IconoMicrofono } from "../components/Icons/icons";
 import { karaokeService } from "../services/api";
+import logoAniversary from "../assets/14 Aniversario Logo TSE-1_page-0001.jpg";
 
 function PublicView() {
   const [requests, setRequests] = useState([]);
@@ -32,18 +33,23 @@ function PublicView() {
     <div className="min-h-screen p-4 sm:p-8">
 
       <header className="text-center mb-10">
+        <img
+          src={logoAniversary}
+          alt="TSE Aniversario"
+          className="mx-auto mb-6 w-48 h-48 rounded-full shadow-lg object-cover"
+        />
         <h1 className="text-4xl sm:text-5xl font-extrabold text-accent-green flex items-center justify-center gap-3">
-          <IconoMicrofono size={36} className="text-primary-light" /> Tablero de
+          <IconoMicrofono size={36} className="text-primary-light" />
           Solicitudes de Karaoke
         </h1>
         <p className="text-gray-400 mt-2 text-lg">
-          Envía canciones y gestiona la cola. Los datos se reinician al
-          recargar.
+          Envía tus canciones favoritas y únete a la diversión
         </p>
       </header>
 
       {/* Layout principal (Responsive) */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-6xl mx-auto">
+
         <FormSolicitud onAdd={addRequest}/>
         <ColaSolicitudes requests={requests} />
       </div>
